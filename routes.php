10defebab6b87fb92dbb350f31ce01ad451f16f1
umbracious <63139410+umbracious@users.noreply.github.com>
2025-04-03 16:35:09 -0400

    routing skeleton

--- routes.php
+++ routes.php
@@ -4,10 +4,39 @@ require_once __DIR__.'/router.php';
 
 # A - Gestion des joueurs
 
-#
+## <path>#/gamers/<joueur>
 get('/gamers/$joueur', 'gamers/joueur.php');
 
+## <path>#/gamers/add/<joueur>/<pwd>
+post('/gamers/add/$joueur/$pwd', 'gamers/add.php');
 
+## <path>#/gamers/login/<joueur>/<pwd>
+put('/gamers/login/$joueur/$pwd', 'gamers/login.php');
+
+## <path>#/gamers/logout/<joueur>/<pwd>
+put('gamers/logout/$joueur/$pwd', 'gamers/logout.php');
+
+# B - Consultation admin
+
+## <path>#/admin/top[/<nb>]
+
+## <path>#/admin/delete/joueur/<joueur>
+
+## <path>#/admin/delete/def/<id>
+
+# C - Consultation des définitions
+
+## <path>#/word[/<nb>[/<from>]]
+
+# D- Page Web (2 jeux et 2 interfaces de consultation)
+
+## <path>#/jeu/word/[<lg>[/<time>[/<hint>]]]
+
+## <path>#/jeu/def[/<lg>/[/<time>]]
+
+## <path>#/dump/<step>
+
+## <path>#/doc
 
 // ##################################################
 // ##################################################
